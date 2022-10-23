50d12def2dfd7d6480c264f2988ce513316d22ff
牧大佑 <daisukemaki1003@gmail.com>
2022-10-23 16:10:03 +0900

    [add] usecase room fetch_allのoutputを非同期に修正

--- lib/domain/usecases/room/fetch_all.dart
+++ lib/domain/usecases/room/fetch_all.dart
@@ -1,8 +1,14 @@
-import '../../core/usecase.dart';
+import 'package:fortune_client/domain/entities/models/room.dart';
+
+import '../core/usecase.dart';
 
 abstract class RoomFetchAllUseCase
     implements UseCase<RoomFetchAllParams, RoomFetchAllParams> {}
 
 class RoomFetchAllParams {}
 
-class RoomFetchAllResults {}
+class RoomFetchAllResults {
+  final List<Room> rooms;
+
+  RoomFetchAllResults({required this.rooms});
+}
