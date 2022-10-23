37485ac835ced259c0ced316d8b2655f66c4e7d2
牧大佑 <daisukemaki1003@gmail.com>
2022-10-23 16:45:52 +0900

    #1 [add] usecase room_create 実装

---
+++ lib/domain/usecases/room/create.dart
@@ -0,0 +1,33 @@
+import 'package:fortune_client/domain/entities/enum/age_group.dart';
+
+import '../../entities/models/room.dart';
+import '../core/usecase.dart';
+
+abstract class RoomCreateUseCase
+    implements UseCase<RoomCreateParams, RoomCreateParams> {}
+
+class RoomCreateParams {
+  final String roomName;
+  final String mamberNum;
+  final AgeGroup ageGroup;
+  final String address;
+  final String explanation;
+  final List<String> tag;
+
+  RoomCreateParams({
+    required this.roomName,
+    required this.mamberNum,
+    required this.ageGroup,
+    required this.address,
+    required this.explanation,
+    required this.tag,
+  });
+}
+
+class RoomCreateResults {
+  final Room createdRoom;
+
+  RoomCreateResults({
+    required this.createdRoom,
+  });
+}
