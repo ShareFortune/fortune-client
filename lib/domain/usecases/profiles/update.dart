be3d5a4e21b1ece2071630754b17531c4b5ee30f
牧大佑 <daisukemaki1003@gmail.com>
2022-10-23 19:59:40 +0900

    #1 [add] Profile更新のUsecaseを定義

---
+++ lib/domain/usecases/profiles/update.dart
@@ -0,0 +1,43 @@
+import 'package:fortune_client/domain/entities/models/tag.dart';
+
+import '../../entities/enum/cigarette_frequency.dart';
+import '../../entities/enum/drink_frequency.dart';
+import '../../entities/enum/gender.dart';
+import '../core/usecase.dart';
+
+abstract class ProfileUpdateUseCase
+    implements UseCase<ProfileUpdateParams, ProfileUpdateResults> {}
+
+class ProfileUpdateParams {
+  final String mainImagePath;
+  final String secondImagePath;
+  final String thirdImagePath;
+  final String fourthImagePath;
+  final Gender gender;
+  final int height;
+  final DrinkFrequency drinkFrequency;
+  final CigaretteFrequency cigaretteFrequency;
+  final String selfIntroduction;
+  final int countryId;
+  final int prefectureId;
+  final int cityId;
+  final List<Tag> tag;
+
+  ProfileUpdateParams({
+    required this.mainImagePath,
+    required this.secondImagePath,
+    required this.thirdImagePath,
+    required this.fourthImagePath,
+    required this.gender,
+    required this.height,
+    required this.drinkFrequency,
+    required this.cigaretteFrequency,
+    required this.selfIntroduction,
+    required this.countryId,
+    required this.prefectureId,
+    required this.cityId,
+    required this.tag,
+  });
+}
+
+class ProfileUpdateResults {}
