f9b1f087bef62019b57458d16449fc181b602f23
牧大佑 <daisukemaki1003@gmail.com>
2023-01-25 00:31:35 +0900

    view model

--- lib/view/pages/request/join_requests_confirmation/join_requests_confirmation_view_model.dart
+++ lib/view/pages/request/join_requests_confirmation/join_requests_confirmation_view_model.dart
@@ -1,1 +1,13 @@
+import 'package:fortune_client/view/pages/request/join_requests_confirmation/join_requests_confirmation_state.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+final joinRequestsConfirmationViewModelProvider = StateNotifierProvider<
+    JoinRequestsConfirmationViewModel, JoinRequestsConfirmationState>(
+  (_) => JoinRequestsConfirmationViewModel(),
+);
+
+class JoinRequestsConfirmationViewModel
+    extends StateNotifier<JoinRequestsConfirmationState> {
+  JoinRequestsConfirmationViewModel()
+      : super(const JoinRequestsConfirmationState(joinRequests: []));
+}
