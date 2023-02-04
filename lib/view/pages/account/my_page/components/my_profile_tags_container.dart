9252d829dac534728b434f68525994b0662c3655
牧大佑 <daisukemaki1003@gmail.com>
2023-02-04 16:56:48 +0900

    タグ未設定時のプロフィールテキスト

--- lib/view/pages/account/my_page/components/my_profile_tags_container.dart
+++ lib/view/pages/account/my_page/components/my_profile_tags_container.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:fortune_client/data/model/base/tag/tag.dart';
 import 'package:fortune_client/l10n/locale_keys.g.dart';
 import 'package:fortune_client/view/pages/account/my_page/components/my_profile_container.dart';
+import 'package:fortune_client/view/theme/app_text_theme.dart';
 import 'package:fortune_client/view/theme/app_theme.dart';
 import 'package:fortune_client/view/widgets/other/tag_widget.dart';
 
@@ -26,7 +27,10 @@ class MyProfileTagsContainer extends StatelessWidget {
       trailing: null,
       onTap: onTap,
       child: tags.isEmpty
-          ? Text(LocaleKeys.myPage_profiles_tags_empty.tr())
+          ? Text(
+              LocaleKeys.myPage_profiles_tags_empty.tr(),
+              style: theme.textTheme.h30.paint(theme.appColors.subText3),
+            )
           : Wrap(
               spacing: 10,
               runSpacing: 10,
