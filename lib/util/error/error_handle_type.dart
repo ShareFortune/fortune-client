950fe5475ad5077b30a801bfca41a875a9750f04
牧大佑 <daisukemaki1003@gmail.com>
2023-02-21 22:06:53 +0900

    [add] error handle type

---
+++ lib/util/error/error_handle_type.dart
@@ -0,0 +1,7 @@
+enum ErrorHandleType {
+  dialogWithClose, //閉じるボタンのダイアログ表示
+  dialogWithReload, //再読み込みボタンのダイアログ
+  dialogWithReloadAndCancel, //再読み込み、キャンセルボタンのダイアログ
+  toast, //トースト表示
+  inline, //インライン表示
+}
