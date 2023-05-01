56e531a074cd98171af2b8b85d282c7161455e37
牧大佑 <daisukemaki1003@gmail.com>
2023-05-02 00:58:36 +0900

    fix

--- lib/view/pages/common/update/update_page.dart
+++ lib/view/pages/common/update/update_page.dart
@@ -1,14 +1,18 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:fortune_client/l10n/locale_keys.g.dart';
+import 'package:fortune_client/view/theme/app_theme.dart';
 import 'package:fortune_client/view/widgets/dialog/Info_dialog.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class UpdatePage extends StatelessWidget {
+class UpdatePage extends HookConsumerWidget {
   const UpdatePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(appThemeProvider);
+
     showInfoDialog(
       context,
       LocaleKeys.error_forceUpdate.tr(),
@@ -23,6 +27,6 @@ class UpdatePage extends StatelessWidget {
       },
     );
 
-    return Container(color: Colors.white);
+    return Container(color: theme.appColors.onBackground);
   }
 }
